bfc7690a4f168d65ac7c863b3080db1b89660c4e
Jinmin Goh <eric970901@gmail.com>
2020-04-13 05:56:35 +0900

    Codeforces Round #633 Div.2 - problem A solution, accepted

---
+++ #633_Div_2/A.py
@@ -0,0 +1,17 @@
+# Contest No.: 633
+# Problem No.: A
+# Solver:      JEMINI
+# Date:        20200412
+
+import sys
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(sys.stdin.readline().strip())
+        print(n)
+            
+    return
+
+if __name__ == "__main__":
+    main()

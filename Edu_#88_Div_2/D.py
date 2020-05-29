1ce8ddcf37f8f8b6143c1c477781a94ecb35c614
Jinmin Goh <eric970901@gmail.com>
2020-05-30 00:35:35 +0900

    Educational Codeforces Round 88 Div.2 - problem D solution, accepted

--- Edu_#88_Div_2/D.py
+++ Edu_#88_Div_2/D.py
@@ -7,22 +7,20 @@ import sys
 import heapq
 
 # wrong answer
-# answer uses DP, with O(61 * n) space
+# answer uses DP, with O(61 * n) time
 def main():
     n = int(input())
     nums = list(map(int, sys.stdin.readline().split()))
     ans = 0
-    maxVal = [nums[0], 0]
-    sumList = [0] * n
-    sumList[0] = nums[0]
-    for i in range(1, n):
-        sumList[i] = max(0, sumList[i - 1] + nums[i])
-        if sumList[i] == 0 or maxVal[0] < nums[i]:
-            maxVal = [nums[i], i]
-        ans = max(ans, sumList[i] - max(0, maxVal[0]))
-        #print(sumList)
-        #print(maxVal)
-    #print(sumList)
+    for i in range(-30, 31):
+        tempAns = 0
+        for j in range(n):
+            tempAns += nums[j]
+            if nums[j] > i:
+                tempAns = -31 * n
+            ans = max(ans, tempAns - i)
+            if tempAns < 0:
+                tempAns = 0
     print(ans)
     return
 

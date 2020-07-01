4fccf0722ce35c4a7a074c96ec788f798ab156bb
Jinmin Goh <eric970901@gmail.com>
2020-07-02 05:10:34 +0900

    Codeforces Round #654 Div.2 - problem A solution, AC

---
+++ #654_Div_2/A.py
@@ -0,0 +1,17 @@
+# Contest No.: 654
+# Problem No.: A
+# Solver:      JEMINI
+# Date:        20200701
+
+import sys
+import heapq
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        print((n + 1) // 2)
+    return
+
+if __name__ == "__main__":
+    main()
